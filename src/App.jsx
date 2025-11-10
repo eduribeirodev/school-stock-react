cc2ee78f53b9b7b388507d3e8c01cc2a7c7d1a2c
Eduardo Ribeiro Alves de Oliveira <eduardoribeiro0912@gmail.com>
2025-11-10 14:50:29 -0300

    feat(routes): implement routing in App.jsx and main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { createBrowserRouter, Navigate } from "react-router-dom";
 
-function App() {
-  const [count, setCount] = useState(0)
+import Layout from "./components/layout/Layout";
+import Home from "./pages/home/Home";
+import Products from "./pages/products/Product";
+import Categories from "./pages/categories/Category";
+import Login from "./pages/login/Login";
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+const routes = createBrowserRouter([
+  {
+    path: "/login",
+    element: <Login />, 
+  },
+  {
+    path: "/",
+    element: <Navigate to="/login" />, 
+  },
+  {
+    path: "/stock-control",
+    element: <Layout />, 
+    children: [
+      { path: "dashboard", element: <Home /> },
+      { path: "products", element: <Products /> },
+      { path: "Categories", element: <Categories /> },
+    ],
+  },
+]);
 
-export default App
+export default routes;
