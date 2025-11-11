dc67af77f118b0d4a8abaf03e710948b72a92221
Eduardo Ribeiro Alves de Oliveira <eduardoribeiro0912@gmail.com>
2025-11-11 18:27:20 -0300

    style(home): adjust layout and spacing of component

--- src/components/cards/HomeCard.jsx
+++ src/components/cards/HomeCard.jsx
@@ -3,13 +3,13 @@
 
 export default function HomeCard({ title: Title, icon: Icon, p1:P1, quantidade:Qtde }) {
   return (
-    <div className="  border border-gray-200  w-[19%]  rounded-xl bg-white/80 place-content-center py-4 shadow-xl">
+    <div className="  border border-gray-200  w-[22%]  justify-around  rounded-xl bg-white/80 place-content-center py-4 shadow-xl">
       
-      <div className="flex items-center justify-center pr-3 ">
+      <div className="flex items-center justify-center">
         <p className=" px-4 text-black/80">{Title}</p>
         <Icon size={16} className="text-2xl text-black/60 p" />
       </div>
-      <p className=" flex text-3xl  text-black/80  place-content-center py-4 ">
+      <p className=" flex text-3xl  text-black/80 text-center justify-center py-4  ">
         {Qtde}
       </p>
         <p className="text-gray-400 pl-3 text-center">
