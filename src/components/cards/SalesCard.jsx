b87579e5d45401add853d053f44620599d4beacb
Eduardo Ribeiro Alves de Oliveira <eduardoribeiro0912@gmail.com>
2025-11-13 15:06:00 -0300

    style (sale) : change the card styling

--- src/components/cards/SalesCard.jsx
+++ src/components/cards/SalesCard.jsx
@@ -8,20 +8,20 @@ export default function SalesCard({ category, productName, price, stock, onAddCl
     const formattedHex = categoryHex ? `#${categoryHex.replace('#', '')}` : '#6b7280'; 
 
     return (
-        <div className="border border-gray-200 rounded-xl bg-white shadow-md overflow-hidden hover:shadow-lg transition-all w-full max-w-60">
+        <div className="border border-white rounded-xl bg-white shadow-md overflow-hidden hover:shadow-lg transition-all w-full max-w-60">
             
-            <div className="relative bg-gray-100 h-16 flex items-center p-3">
+            <div className="relative bg-white h-16 flex items-center p-3">
                 
                 <div 
                     style={{ backgroundColor: formattedHex }} 
-                    className={`px-3 py-1 text-white text-xs font-semibold rounded-full`}
+                    className={`px-3 py-1 text-white text-xs font-semibold rounded-full `}
                 >
                     {category}
                 </div>
             </div>
 
             <div className="p-3">
-                <h3 className="text-base font-semibold text-gray-800 whitespace-nowrap overflow-hidden text-ellipsis mb-2">
+                <h3 className="text-2xl font-semibold   text-black/75 whitespace-nowrap overflow-hidden text-center mb-2 pb-3">
                     {productName}
                 </h3>
 
